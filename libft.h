75d137346221d8cafc8e6183baa618c9102b9503
Kilian Tintim Goncalves <ktintim-@c1r16s2.42lausanne.ch>
2024-10-02 18:01:46 +0200

    norminette de ta mer la pute

--- libft.h
+++ libft.h
@@ -31,7 +31,8 @@ int				ft_memcmp(const void *s1, const void *s2, size_t n);
 int				ft_toupper(char c);
 int				ft_tolower(char c);
 void			*ft_memset(void *ptr, int c, size_t len);
-void			*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void			*ft_memcpy(void *restrict dst, \
+				const void *restrict src, size_t n);
 void			*ft_memove(void *dst, const void *src, size_t len);
 void			*ft_memchr(const void *s, int c, size_t n);
 void			bzero(void *s, size_t n);
