98e65b5833f6c13055e0f2aa87c18ca9be12e6e8
Kilian Tintim Goncalves <ktintim-@c1r16s2.42lausanne.ch>
2024-10-02 10:29:47 +0200

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -9,3 +9,32 @@
 /*   Updated: 2024/10/01 15:28:30 by ktintim-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <limits.h>
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	negative;
+	int	result;
+
+	result = 0;
+	negative = 1;
+	i = 0;
+	while (str[i] == ' ')
+		i++;
+	if (str[i] == '-')
+		negative *= -1;
+	i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		if (result > (INT_MAX - (str[i] - '0')) / 10)
+		{
+			if (negative == -1)
+				return (INT_MIN);
+			return (INT_MAX);
+		}
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * negative);
+}		
